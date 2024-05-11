796dd010f36345b688444cc30663730e6aabeed0
Kaveesha Maharambage <153258780+Kaveesha00@users.noreply.github.com>
2024-05-11 20:21:01 +0530

    kaveesha

--- app/src/main/java/com/example/todolistappication/MainActivity.kt
+++ app/src/main/java/com/example/todolistappication/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -16,6 +18,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val recyclerView:RecyclerView=findViewById(R.id.rvToDoList)
+        val todoAdapter=TodoAdapter()
+        recyclerView.adapter = todoAdapter
+        recyclerView.layoutManager=LinearLayoutManager(this)
 
     }
 
--- app/src/main/java/com/example/todolistappication/TodoAdapter.kt
+++ app/src/main/java/com/example/todolistappication/TodoAdapter.kt
@@ -2,26 +2,20 @@ package com.example.todolistappication
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView.Adapter
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import androidx.recyclerview.widget.RecyclerView
 
-class TodoAdapter {
+class TodoAdapter : RecyclerView.Adapter<TodoViewHolder>() {
 
-    class TodoAdapter:Adapter<TodoViewHolder>(){
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
-           val view =LayoutInflater.from(parent.context).inflate(R.layout.view_item,parent,false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.view_item, parent, false)
+        return TodoViewHolder(view)
+    }
 
-            return TodoViewHolder(view)
-        }
+    override fun getItemCount(): Int {
+        return 1
+    }
 
-        override fun getItemCount(): Int {
-           return 1
-        }
-
-        override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
-
-            holder.cbTodo.text="Sample Text"
-        }
-
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        holder.cbTodo.text = "Sample Text"
     }
 }
