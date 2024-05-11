191b4b346e28e52590376407012b1525e7a5f343
Kaveesha Maharambage <153258780+Kaveesha00@users.noreply.github.com>
2024-05-11 14:46:49 +0530

    project added to github

--- app/src/main/java/com/example/todolistappication/MainActivity.kt
+++ app/src/main/java/com/example/todolistappication/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.todolistappication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView :TextView=findViewById(R.id.textView)
+        val button:Button=findViewById(R.id.button)
+
+       CoroutineScope(Dispatchers.Main).launch {
+           counter(textView)
+       }
+
+        button.setOnClickListener{
+            Toast.makeText(this,"This a button click",Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private suspend fun counter (view: TextView){
+        var x=0
+
+        while (true){
+            view.text=x.toString()
+            delay(1000)
+            x++
+        }
     }
 }
