c7f8758504d11e3b0c3dc370462804cde9eab3a0
Kaveesha Maharambage <153258780+Kaveesha00@users.noreply.github.com>
2024-05-11 23:28:57 +0530

    kaveesha

--- app/src/main/java/com/example/todolistappication/TodoAdapter.kt
+++ app/src/main/java/com/example/todolistappication/TodoAdapter.kt
@@ -1,21 +1,31 @@
 package com.example.todolistappication
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class TodoAdapter : RecyclerView.Adapter<TodoViewHolder>() {
 
+    lateinit var context:Context
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.view_item, parent, false)
+
+       context=parent.context
+
         return TodoViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        return 1
+        return 100
     }
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
         holder.cbTodo.text = "Sample Text"
+        holder.ivDelete.setOnClickListener{
+            Toast.makeText(context,"Hello from Button $position",Toast.LENGTH_LONG).show()
+        }
     }
 }
---
+++ app/src/main/java/com/example/todolistappication/database/Todo.kt
@@ -0,0 +1,8 @@
+package com.example.todolistappication.database
+
+
+data class Todo(
+    var item:String?
+){
+    var id:Int?=null
+}
