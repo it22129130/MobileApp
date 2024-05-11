fb957e9e7589b5037493e2c37dc084b7c356f14d
Kaveesha Maharambage <153258780+Kaveesha00@users.noreply.github.com>
2024-05-11 16:09:13 +0530

    kaveesha

--- app/src/main/java/com/example/todolistappication/TodoAdapter.kt
+++ app/src/main/java/com/example/todolistappication/TodoAdapter.kt
@@ -1,4 +1,26 @@
 package com.example.todolistappication
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
 class TodoAdapter {
+
+    class TodoAdapter:Adapter<TodoViewHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+           val view =LayoutInflater.from(parent.context).inflate(R.layout.view_item,parent,false)
+
+            return TodoViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
+        }
+
+        override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+
+    }
 }
