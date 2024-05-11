6d97f1cd62a9da81922bedb42c7251755897d879
Kaveesha Maharambage <153258780+Kaveesha00@users.noreply.github.com>
2024-05-11 15:59:18 +0530

    kaveesha

--- app/src/main/java/com/example/todolistappication/TodoViewHolder.kt
+++ app/src/main/java/com/example/todolistappication/TodoViewHolder.kt
@@ -1,5 +1,14 @@
 package com.example.todolistappication
 
-class TodoViewHolder {
+import android.view.View
+import android.widget.CheckBox
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class TodoViewHolder(view:View):ViewHolder(view) {
+
+    val cbTodo:CheckBox=view.findViewById(R.id.cdTodo)
+    val ivDelete:ImageView=view.findViewById(R.id.ivDelete)
+
 
 }
